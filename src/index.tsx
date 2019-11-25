f442529c75160f5ade640b640140d69f072bc0a4
Christoph Martel <chris@codeways.org>
2019-11-25 22:17:40 +0100

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createStore } from "redux";
 
 import "./styles.css";
 
-function counter(state = 0, action) {
+function counter(state = 0, action: { type: any; }) {
   switch (action.type) {
     case "INCREMENT":
       state = state + 1;
@@ -25,11 +25,25 @@ store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "DECREMENT" });
 store.dispatch({ type: "INCREMENT" });
 
+
+type DotProps = {
+  color: "#FF0000"
+}
+
+const RedDot = () => 
+  <svg width="640" height="480">
+  <g>
+    <ellipse ry="25" rx="25" id="svg_2" cy="132" cx="136" stroke-width="5" stroke="#000000" fill={ `DotProps.color` }/>
+  </g>
+</svg> 
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some happen!</h2>
+        <RedDot></RedDot>
     </div>
   );
 }
